c398eded28288666bd96029af3404f462d4232ee
VanshikaChaudhary119 <cvanshika979@gmail.com>
2026-09-24 11:35:13 -0700

    Add logout functionality by adding header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <a href="home">Home</a>
+        <a href="home">Add Restaurant</a>
+        <a href="home">Update Restaurant</a>
+        <a v-on:click="logout" href="#" >Logout</a>
+    </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    logout()
+    {
+       localStorage.clear();
+       this.$router.push({name:'Login'})
+    }
+  }
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,15 @@
 <template>
+    <Header/>
 <h1>Hello,Welcome on Home Page</h1>
 </template>
 
 <script>
+import Header from './Header.vue';
+
 export default{
+    components:{
+Header
+    },
 mounted()
     {
         let user = localStorage.getItem('user-info');
